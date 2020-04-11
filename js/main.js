3ddb6f9632ca045681de3b4f5ee39affa611005f
Romaine Smith <thisisromaine@gmail.com>
2020-04-11 10:45:53 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Jul 28, 2020 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  // Math.floor function takes the biggest number which is equal to or less than the number generated in the brackets
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="countdown", replacing any content within the html tag
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "The Time Has Come!";
+  }
+}, 1000);
